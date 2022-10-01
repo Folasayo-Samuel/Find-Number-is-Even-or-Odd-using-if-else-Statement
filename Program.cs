cd25d542a51340a5e3031cc030f097e8ee10a9fe
Folasayo Samuel <folasayosamuel@gmail.com>
2022-10-01 20:22:16 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EvenOddNumber
+{
+	public class Program
+	{
+		public static void Main(string[]args)
+		{
+			int n;
+			Console.Write("Enter an integer: ");
+			n = Int32.Parse(Console.ReadLine());
+			
+			if(n%2==0)
+				Console.WriteLine($"{n} is even");
+			else
+				Console.WriteLine($"{n} is odd");
+
+            Console.ReadKey();
+        }
+	}
+}
